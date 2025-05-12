e224c5a4242837c09c0538143fbede1ef7fcd669
hadatttt <hagamer2004@gmail.com>
2025-05-13 01:51:12 +0700

    update detial

--- app/src/main/java/com/example/foodorderapp/ui/DetailFoodActivity.java
+++ app/src/main/java/com/example/foodorderapp/ui/DetailFoodActivity.java
@@ -3,6 +3,7 @@ package com.example.foodorderapp.ui;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.example.foodorderapp.R;
 import com.example.foodorderapp.model.FoodModel;
 import com.example.foodorderapp.service.FoodService;
@@ -62,26 +64,31 @@ public class DetailFoodActivity extends AppCompatActivity {
 
                 if (currentFood != null) {
                     // Lấy storeId
+                    String urlImage = document.getString("imageUrl");
                     Long storeIdLong = document.getLong("storeId");
                     int storeId = storeIdLong != null ? storeIdLong.intValue() : -1;
-
                     // Lấy rating
                     Double ratingDouble = document.getDouble("rating");
                     float rating = ratingDouble != null ? ratingDouble.floatValue() : 0.1f;
+                    float a = currentFood.getFoodId();
 
                     // Lấy name
                     String name = document.getString("name");
-                    String decs = document.getString("caption");
+//                    String decs = document.getString("caption");
 
                     // Lấy price
                     Double priceDouble = document.getDouble("price");
                     double price = priceDouble != null ? priceDouble : 0.3;
+                    ImageView foodImageView = findViewById(R.id.imgFood);
+                    Glide.with(this)
+                            .load(urlImage)
+                            .into(foodImageView);
                     TextView foodName = findViewById(R.id.tvFoodName);
                     TextView ratting = findViewById(R.id.tvRate);
                     TextView cap = findViewById(R.id.tvDesc);
                     foodName.setText(name);
                     ratting.setText(String.valueOf(rating));  // Đổi từ float sang String
-                    cap.setText(decs);
+//                    cap.setText(decs);
 
                     Log.d("DetailFood", "storeId: " + storeId);
                     Log.d("DetailFood", "name: " + name);
@@ -93,13 +100,13 @@ public class DetailFoodActivity extends AppCompatActivity {
                         if (shopTask.isSuccessful() && shopTask.getResult() != null && !shopTask.getResult().isEmpty()) {
                             DocumentSnapshot shopDoc = shopTask.getResult().getDocuments().get(0);
 
-                            String shopName = shopDoc.getString("name");
+                            String shopName = shopDoc.getString("shopName");
                             String address = shopDoc.getString("address");
 
                             Log.d("DetailShop", "Shop Name: " + shopName);
                             Log.d("DetailShop", "Address: " + address);
 
-                            TextView shopn = findViewById(R.id.tvShopName);
+                            TextView shopn = findViewById(R.id.tvStore);
                             shopn.setText(shopName);
                         } else {
                             Toast.makeText(this, "Không tìm thấy cửa hàng", Toast.LENGTH_SHORT).show();
