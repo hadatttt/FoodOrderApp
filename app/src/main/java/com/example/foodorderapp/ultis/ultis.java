75bced5d12d4cba800fec8e434d6a90ee8240903
trhieu27 <trunghieu271004@gmail.com>
2025-05-17 20:35:47 +0700

    update

--- app/src/main/java/com/example/foodorderapp/ultis/ultis.java
+++ app/src/main/java/com/example/foodorderapp/ultis/ultis.java
@@ -2,5 +2,6 @@ package com.example.foodorderapp.ultis;
 
 public class ultis {
     // Địa chỉ WebSocket server
-    public static final String WEBSOCKET_URL = "ws://192.168.150.107:8080";
+    public static final String WEBSOCKET_URL = "wss://serverfoororder-production.up.railway.app";
+
 }
