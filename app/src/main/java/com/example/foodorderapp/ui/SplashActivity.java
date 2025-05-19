1e6e63a189048fb36c725a9c52f0e876600de5e2
Minhduc252 <minhduc.fwrks@gmail.com>
2025-05-20 01:07:00 +0700

    Update userflow

--- app/src/main/java/com/example/foodorderapp/ui/SplashActivity.java
+++ app/src/main/java/com/example/foodorderapp/ui/SplashActivity.java
@@ -10,6 +10,8 @@ import androidx.core.splashscreen.SplashScreen;
 
 import com.example.foodorderapp.IntroActivity;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
     private boolean loadingSplash = true;
@@ -21,6 +23,29 @@ public class SplashActivity extends AppCompatActivity {
         setUpSplashScreen();
     }
 
+//    private void setUpSplashScreen() {
+//        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+//        splashScreen.setKeepOnScreenCondition(() -> loadingSplash);
+//
+//        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+//            loadingSplash = false;
+//            FirebaseApp.initializeApp(this);
+//
+//            Intent intent = new Intent(this, IntroActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }, 2000);
+//
+//        splashScreen.setOnExitAnimationListener(splashScreenView -> {
+//            splashScreenView.getView().setAlpha(1f);
+//            splashScreenView.getView().animate()
+//                    .alpha(0f)
+//                    .setDuration(300)
+//                    .withEndAction(splashScreenView::remove)
+//                    .start();
+//        });
+//    }
+
     private void setUpSplashScreen() {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         splashScreen.setKeepOnScreenCondition(() -> loadingSplash);
@@ -28,9 +53,17 @@ public class SplashActivity extends AppCompatActivity {
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             loadingSplash = false;
             FirebaseApp.initializeApp(this);
-
-            Intent intent = new Intent(this, IntroActivity.class);
-            startActivity(intent);
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            if (currentUser == null) {
+                Intent intent = new Intent(this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                // User is logged in, proceed to main screen
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
             finish();
         }, 2000);
 
